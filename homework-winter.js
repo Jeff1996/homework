0fe2b501390dabeb55d3941fa4edc048f65fa49d
Jeff1996 <1468401336@qq.com>
2016-02-29 18:46:28 +0800

    git tracks changes

--- homework-winter.js
+++ homework-winter.js
@@ -2,14 +2,18 @@
 function myFunction()
 {
 document.getElementById("title").innerHTML="小池";
-document.getElementById("author").innerHTML="[宋] &nbsp &nbsp 杨万里";
+document.getElementById("author").innerHTML="[宋] &nbsp &nbsp &nbsp 杨万里";
 document.getElementById("content1").innerHTML="泉眼无声惜细流，树阴照水爱晴柔。";
 document.getElementById("content2").innerHTML="小荷才露尖尖角，早有蜻蜓立上头。";
 }
-/*导航当前页高亮*/
+/*导航当前页高亮
 var obj=null;
 var As=document.getElementById('topnav').getElementsByTagName('a');
 obj = As[0];
-for(i=1;i<As.length;i++){if(window.location.href.indexOf(As[i].href)>=0)
-obj=As[i];}
-obj.id='topnav_current'
+for(i=1;i<As.length;i++)
+{
+if(window.location.href.indexOf(As[i].href)>=0)
+obj=As[i];
+}
+obj.id='topnav_current'*/
+
